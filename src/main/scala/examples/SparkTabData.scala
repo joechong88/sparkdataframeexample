52c95d64e2d493ffa5cd4d4bbd1a1800f648bafd
joechong88 <joechong@gmail.com>
2016-07-15 17:18:16 +0800

    SparkTabData-v2

--- src/main/scala/examples/SparkTabData.scala
+++ src/main/scala/examples/SparkTabData.scala
@@ -10,4 +10,14 @@ import sqlContext.implicits._
 // import Spark SQL data types and Row.
 import org.apache.spark.sql._
 
-// loda the data into a new RDD
+// load the data into a new RDD
+val ebayText = sc.textFile("hdfs://192.168.0.94:8020/user/jchong1/sparkdataframeexample/ebay.csv")
+
+// return the first element in this RDD
+ebayText.first()
+
+// define the schema using a case class
+case class Auction(auctionid: String, bid: Float, bidtime: Float, 
+	bidder: String, bidderrate: Integer, openbid: Float, price: Float, 
+	item: String, daystolive: Integer)
+	
